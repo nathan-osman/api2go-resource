44cb0665a625f45b05d0078d779047268204ccad
Nathan Osman <nathan@quickmediasolutions.com>
2018-02-21 23:15:51 -0800

    Add apply method for applying filters.

---
+++ apply.go
@@ -0,0 +1,28 @@
+package resource
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/manyminds/api2go"
+)
+
+var ErrInvalidParameter = errors.New("invalid parameter")
+
+// apply takes the query parameters from a request and applies them to an SQL
+// query.
+func (r *Resource) apply(req api2go.Request) (*gorm.DB, error) {
+	c := r.DB
+loop:
+	for k, v := range req.QueryParams {
+		for _, f := range r.Fields {
+			if f == k {
+				c = c.Where(fmt.Sprintf("%s = ?", k), v[0])
+				continue loop
+			}
+		}
+		return nil, ErrInvalidParameter
+	}
+	return c, nil
+}
--- resource.go
+++ resource.go
@@ -7,6 +7,12 @@ import (
 // Resource implements the interfaces necessary to use a GORM model with the
 // api2go package.
 type Resource struct {
-	DB   *gorm.DB
+	// DB is a pointer to an open database connection.
+	DB *gorm.DB
+
+	// Type is an instance of the model for this resource.
 	Type interface{}
+
+	// Fields is a list of valid field names for filtering.
+	Fields []string
 }
