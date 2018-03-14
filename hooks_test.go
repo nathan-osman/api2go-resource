3dbb7465cdb0e55b657c7743d65b760d5c3fcef7
Nathan Osman <nathan@quickmediasolutions.com>
2018-03-14 13:34:08 -0700

    Further refinement to hook tests.

--- hooks_test.go
+++ hooks_test.go
@@ -1,34 +1,58 @@
 package resource
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/jinzhu/gorm"
 	"github.com/manyminds/api2go"
 )
 
-func TestGetHook(t *testing.T) {
+func TestHooks(t *testing.T) {
 	c, a, err := initDatabase()
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer c.Close()
-	for i := 0; i < 3; i++ {
-		_, err := createArticle(c)
-		if err != nil {
-			t.Fatal(err)
-		}
+	article, err := createArticle(c)
+	if err != nil {
+		t.Fatal(err)
 	}
+	var (
+		globalHookInvoked = false
+		getHookInvoked    = false
+		setHookInvoked    = false
+	)
 	a.AddResource(&Article{}, &Resource{
 		DB:   c,
 		Type: &Article{},
+		GlobalHooks: []GlobalHook{
+			func(Action, api2go.Request) error {
+				globalHookInvoked = true
+				return nil
+			},
+		},
 		GetHooks: []GetHook{
 			func(c *gorm.DB, req api2go.Request) *gorm.DB {
-				return c.Limit("2")
+				getHookInvoked = true
+				return c
+			},
+		},
+		SetHooks: []SetHook{
+			func(interface{}, api2go.Request) {
+				setHookInvoked = true
 			},
 		},
 	})
-	if err := verifyCount(a, 2); err != nil {
+	_, err = sendRequest(
+		a,
+		http.MethodGet,
+		fmt.Sprintf("/articles/%d", article.ID),
+		nil,
+		http.StatusOK,
+	)
+	if err != nil {
 		t.Fatal(err)
 	}
 }
